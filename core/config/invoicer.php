3c0b86f18e4751a833146b3ca60d5fae821051a8
Christopher Boyd <chris@boyddesignstudio.com>
2015-06-25 16:19:01 -0400

    Initial commit

---
+++ core/config/invoicer.php
@@ -0,0 +1,4 @@
+<?php return array('install' => true,
+                                          'version' => 2,
+                                          'install date' => '2015-06-25 13:26:16',
+                                        );
